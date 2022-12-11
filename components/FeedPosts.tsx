61e559be122b7c3d1c2dec588814aabe95ab31f4
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-11 12:11:37 +0700

    post header & img

--- components/FeedPosts.tsx
+++ components/FeedPosts.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline"
 
 export default function FeedPosts() {
   const posts = [
@@ -12,7 +13,7 @@ export default function FeedPosts() {
     {
       id: 2,
       userName: "ninggen",
-      userImg: "https://i.pravatar.cc/150?img=14",
+      userImg: "https://i.pravatar.cc/150?img=19",
       img: "https://phantom-marca.unidadeditorial.es/36b733ae69cb4607319886fbce9a14d0/resize/1320/f/jpg/assets/multimedia/imagenes/2022/10/31/16672255275951.jpg",
       caption: "This is a caption to hehe"
     }
@@ -20,11 +21,16 @@ export default function FeedPosts() {
   return (
     <div>
       {posts.map((post) => (
-        <div key={post.id} className="p-5 ">
-          <h1>{post.userName}</h1>
-          <div className="flex items-center w-96 aspect-square">
-            <img src={post.img} className="w-96 aspect-square" />
+        <div key={post.id} className="w-[450px] bg-white my-7 shadow">
+          <div className="flex items-center border-b-2 p-3">
+            <img
+              src={post.userImg}
+              className="w-10  border-2 p-[1.5px]  rounded-full aspect-square object-cover"
+            />
+            <h1 className="text-lg ml-4">{post.userName}</h1>
+            <EllipsisHorizontalIcon className="h-5 ml-auto pr-1" />
           </div>
+          <img src={post.img} alt="" className="w-fit aspect-square" />
         </div>
       ))}
     </div>
