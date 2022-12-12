a47eb829408510f2d23e1d9fadd6cfc802c7ed66
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-12 07:26:36 +0700

    initialize react model in UploadModal component

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,9 +1,24 @@
 import { useRecoilState } from "recoil"
 import { modalState } from "../atom/modalAtom"
+// @ts-ignore
+import Modal from "react-modal"
 
 export default function UploadModal() {
   const [open, setOpen] = useRecoilState(modalState)
-  return <div>UploadModal
-  {open && <h1>The modal is open</h1>}
-  </div>
+  return (
+    <div>
+      UploadModal
+      {open && (
+        <Modal
+          className="max-w-lg w-[90%] h-[300px] absolute top-56 left-1/2 -translate-x-1/2 bg-white ring-0 border-2 rounded shadow-md"
+          isOpen={open}
+          onRequestClose={() => setOpen(false)}
+        >
+          <div className="flex flex-col h-full justify-center items-center">
+            <h1>Modal</h1>
+          </div>
+        </Modal>
+      )}
+    </div>
+  )
 }
