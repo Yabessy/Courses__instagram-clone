e26eb9ff5db18bc971b2c6eadf46c4e40e029818
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-12 13:58:09 +0700

    show comment from firestore

---
+++ components/FeedPost.tsx
@@ -0,0 +1,117 @@
+import {
+  EllipsisHorizontalIcon,
+  HeartIcon,
+  ChatBubbleLeftIcon,
+  BookmarkIcon,
+  FaceSmileIcon
+} from "@heroicons/react/24/outline"
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore"
+import { useSession } from "next-auth/react"
+import { useEffect, useState } from "react"
+import Moment from "react-moment"
+import { db } from "../firebase"
+
+export default function FeedPost({ id, profileImg, username, image, caption }: any) {
+  const [comment, setComment] = useState("")
+  const [comments, setComments] = useState([])
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts", id, "comments"), orderBy("timestamp", "desc")),
+      (snapshot: any) => {
+        setComments(snapshot.docs)
+      }
+    )
+  }, [db, id])
+  async function sendComment(e: any) {
+    e.preventDefault()
+    const commentToSend = comment
+    setComment("")
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      // @ts-ignore
+      username: session.user.username,
+      // @ts-ignore
+      profileImg: session.user.image,
+      timestamp: serverTimestamp()
+    })
+  }
+  const { data: session } = useSession()
+  return (
+    // @ts-ignore
+    <div className="w-full bg-white my-7 shadow">
+      {/* PostHeader */}
+      <div className="flex items-center border-b-2 p-3">
+        <img
+          // @ts-ignore
+          src={profileImg}
+          className="w-10  border-2 p-[1.5px]  rounded-full aspect-square object-cover"
+        />
+        {/* @ts-ignore */}
+        <h1 className="text-lg ml-4">{username}</h1>
+        <EllipsisHorizontalIcon className="h-5 ml-auto pr-1" />
+      </div>
+
+      {/* PostImage */}
+      {/* @ts-ignore */}
+      <img src={image} alt="" className="w-full aspect-square object-cover" />
+
+      {/* PostButtons */}
+      {session && (
+        <div className="flex justify-between p-4">
+          <div className="flex space-x-1">
+            <HeartIcon className="btn" />
+            <ChatBubbleLeftIcon className="btn" />
+          </div>
+          <BookmarkIcon className="btn" />
+        </div>
+      )}
+
+      {/* PostComment */}
+      <p className="px-5 py-2 truncate">
+        {/* @ts-ignore */}
+        <span className="font-medium mr-1">{username}</span>
+        {/* @ts-ignore */}
+        {caption}
+      </p>
+      {comments.length > 0 && (
+        <div className="mx-10 max-h-24 scrollbar-thin overflow-y-scroll">
+          {comments.map((comment: any) => (
+            <div key={comment.id} className="flex items-center space-x-2 p-3">
+              <img
+                // @ts-ignore
+                src={comment.data().profileImg}
+                className="w-6 h-6 rounded-full object-cover"
+              />
+              <p className="font-medium">{comment.data().username}</p>
+              <p className="flex-1 truncate">{comment.data().comment}</p>
+              <Moment fromNow>{comment.data().timestamp?.toDate()}</Moment>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {/* PostInputBox */}
+      {session && (
+        <form action="" className="flex relative items-center">
+          <FaceSmileIcon className="btn absolute left-2" />
+          <input
+            type="text"
+            placeholder="Enter your comment"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            className="pl-10 pr-12 border-none focus:ring-0 w-full"
+          />
+          <button
+            disabled={!comment.trim()}
+            type="submit"
+            // @ts-ignore
+            onClick={sendComment}
+            className="disabled:text-gray-200 absolute right-2 text-blue-500"
+          >
+            post
+          </button>
+        </form>
+      )}
+    </div>
+  )
+}
--- components/FeedPosts.tsx
+++ components/FeedPosts.tsx
@@ -1,32 +1,10 @@
-import {
-  EllipsisHorizontalIcon,
-  HeartIcon,
-  ChatBubbleLeftIcon,
-  BookmarkIcon,
-  FaceSmileIcon
-} from "@heroicons/react/24/outline"
 import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import { db } from "../firebase"
-import { useSession } from "next-auth/react"
+import FeedPost from "./FeedPost"
 
 export default function FeedPosts() {
-  const { data: session } = useSession()
   const [posts, setPosts] = useState([])
-  const [comment, setComment] = useState("")
-  async function sendComment(e: any, id: string) {
-    e.preventDefault()
-    const commentToSend = comment
-    setComment("")
-    await addDoc(collection(db, "posts", id, "comments"), {
-      comment: commentToSend,
-      // @ts-ignore
-      username: session.user.username,
-      // @ts-ignore
-      profileImg: session.user.image,
-      timestamp: serverTimestamp()
-    })
-  }
   useEffect(() => {
     const unsubscribe = onSnapshot(
       query(collection(db, "posts"), orderBy("timestamp", "desc")),
@@ -39,66 +17,20 @@ export default function FeedPosts() {
   return (
     <div>
       {posts.map((post) => (
-        // @ts-ignore
-        <div key={post.id} className="w-full bg-white my-7 shadow">
-          {/* PostHeader */}
-          <div className="flex items-center border-b-2 p-3">
-            <img
-              // @ts-ignore
-              src={post.data().profileImg}
-              className="w-10  border-2 p-[1.5px]  rounded-full aspect-square object-cover"
-            />
-            {/* @ts-ignore */}
-            <h1 className="text-lg ml-4">{post.data().username}</h1>
-            <EllipsisHorizontalIcon className="h-5 ml-auto pr-1" />
-          </div>
-
-          {/* PostImage */}
-          {/* @ts-ignore */}
-          <img src={post.data().image} alt="" className="w-full aspect-square object-cover" />
-
-          {/* PostButtons */}
-          {session && (
-            <div className="flex justify-between p-4">
-              <div className="flex space-x-1">
-                <HeartIcon className="btn" />
-                <ChatBubbleLeftIcon className="btn" />
-              </div>
-              <BookmarkIcon className="btn" />
-            </div>
-          )}
-
-          {/* PostComment */}
-          <p className="px-5 py-2 truncate">
-            {/* @ts-ignore */}
-            <span className="font-medium mr-1">{post.data().username}</span>
-            {/* @ts-ignore */}
-            {post.data().caption}
-          </p>
-
-          {/* PostInputBox */}
-          {session && (
-            <form action="" className="flex relative items-center">
-              <FaceSmileIcon className="btn absolute left-2" />
-              <input
-                type="text"
-                placeholder="Enter your comment"
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                className="pl-10 pr-12 border-none focus:ring-0 w-full"
-              />
-              <button
-                disabled={!comment.trim()}
-                type="submit"
-                // @ts-ignore
-                onClick={(e) => sendComment(e, post.id)}
-                className="disabled:text-gray-200 absolute right-2 text-blue-500"
-              >
-                post
-              </button>
-            </form>
-          )}
-        </div>
+        <FeedPost
+          // @ts-ignore
+          key={post.id}
+          // @ts-ignore
+          id={post.id}
+          // @ts-ignore
+          profileImg={post.data().profileImg}
+          // @ts-ignore
+          image={post.data().image}
+          // @ts-ignore
+          caption={post.data().caption}
+          // @ts-ignore
+          username={post.data().username}
+        />
       ))}
     </div>
   )
