88db86a9efa977dfa3daa109d0bf6dab801162a9
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-13 10:42:54 +0700

    try fix csr error

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,7 +31,6 @@ export default function Header() {
           if (docSnap.exists()) {
             // @ts-ignore
             setCurrentUser(docSnap.data())
-            console.log(currentUser)
           }
         }
         fetchUser()
