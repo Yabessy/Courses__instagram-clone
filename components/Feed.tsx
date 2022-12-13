451c3efc251b0ee01e71e175c2f0c749d23b1af1
JohanLiebert <johanliebert.outergod@gmail.com>
2022-12-13 13:02:25 +0700

    hide sugestion if no user

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -6,24 +6,31 @@ import { useRecoilState } from "recoil"
 import { userState } from "../atom/userAtom"
 
 export default function Feed() {
-    const [currentUser] = useRecoilState(userState)
+  const [currentUser] = useRecoilState(userState)
 
   return (
-    <main className={`grid ${currentUser ? "grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto" : "grid-cols-1 md:grid-cols-2 md:max-w-3xl mx-auto"}`}>
+    <main
+      className={`grid ${
+        currentUser
+          ? "grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto"
+          : "grid-cols-1 md:grid-cols-2 md:max-w-3xl mx-auto"
+      }`}>
       <section className="md:col-span-2">
         {/* Stories */}
         <FeedStories />
         {/* Post */}
         <FeedPost />
       </section>
-      <section className="md:col-span-1">
-        <div className="fixed w-[380px]">
-          {/* Mini Profile */}
-          <FeedMiniProfile />
-          {/* Suggestions */}
-          <FeedSuggestions />
-        </div>
-      </section>
+      {currentUser && (
+        <section className="hidden md:grid md:col-span-1">
+          <div className="fixed w-[380px]">
+            {/* Mini Profile */}
+            <FeedMiniProfile />
+            {/* Suggestions */}
+            <FeedSuggestions />
+          </div>
+        </section>
+      )}
     </main>
   )
 }
